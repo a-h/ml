0ee408e8bcf17b4013dd14043be7156511db6b33
Adrian Hesketh <adrianhesketh@hushmail.com>
2018-08-26 19:33:00 +0100

    Added simple categorical probability.

---
+++ bayes/calculate.go
@@ -0,0 +1,51 @@
+package bayes
+
+// Category of the data.
+type Category int
+
+// NewDatum creates new data.
+func NewDatum(v interface{}, categories ...Category) Datum {
+	d := Datum{
+		Value:      v,
+		Categories: make(map[Category]interface{}, len(categories)),
+	}
+	for _, c := range categories {
+		d.Categories[c] = struct{}{}
+	}
+	return d
+}
+
+// Datum and its associated categories.
+type Datum struct {
+	Value      interface{}
+	Categories map[Category]interface{}
+}
+
+// Data is a collection.
+type Data []Datum
+
+// Probability of the data being in a category.
+func (d Data) Probability(is Category, given ...Category) float64 {
+	filtered := filter(d, given...)
+	matchesInFilteredData := filter(filtered, is)
+	return float64(len(matchesInFilteredData)) / float64(len(filtered))
+}
+
+func filter(d Data, categories ...Category) (filtered Data) {
+	for _, dm := range d {
+		if !matches(dm, categories...) {
+			continue
+		}
+		filtered = append(filtered, dm)
+	}
+	return
+}
+
+func matches(dm Datum, categories ...Category) bool {
+	for _, c := range categories {
+		if _, ok := dm.Categories[c]; !ok {
+			return false
+		}
+	}
+	return true
+}
---
+++ bayes/calculate_test.go
@@ -0,0 +1,48 @@
+package bayes
+
+import "testing"
+
+func TestProbability(t *testing.T) {
+	categoryDog, categoryCat := Category(1), Category(2)
+
+	categoryBrownHair, categoryBlondeHair := Category(1), Category(2)
+	categoryBlueEyes, categoryBrownEyes := Category(3), Category(4)
+
+	tests := []struct {
+		name     string
+		data     Data
+		is       Category
+		given    []Category
+		expected float64
+	}{
+		{
+			name: "dog or cat",
+			data: []Datum{
+				NewDatum("Felix", categoryCat),
+				NewDatum("Shep", categoryDog),
+			},
+			is:       categoryCat,
+			given:    []Category{},
+			expected: 0.5,
+		},
+		{
+			name: "brown or blue eyes",
+			data: []Datum{
+				NewDatum("John", categoryBlueEyes, categoryBlondeHair),
+				NewDatum("Jane", categoryBlueEyes, categoryBlondeHair),
+				NewDatum("Janet", categoryBlueEyes, categoryBrownHair),
+				NewDatum("June", categoryBrownEyes, categoryBrownHair),
+			},
+			is:       categoryBlondeHair,
+			given:    []Category{categoryBlueEyes},
+			expected: float64(2) / float64(3),
+		},
+	}
+
+	for _, test := range tests {
+		actual := test.data.Probability(test.is, test.given...)
+		if test.expected != actual {
+			t.Errorf("%s: expected %v, got %v", test.name, test.expected, actual)
+		}
+	}
+}
